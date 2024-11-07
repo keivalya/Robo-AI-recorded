569c5ad6a9abd0b90e6a2f20a68bf3a46c418799
Keivalya Pandya <keivalya@Keivalyas-MacBook-Air.local>
2024-11-07 17:33:28 -0500

    change RL, added intropy

---
+++ 4 Reinforcment Learning/intro.py
@@ -0,0 +1,19 @@
+import gymnasium as gym
+import numpy as np
+
+env = gym.make("MountainCar-v0", render_mode="human")
+state = env.reset()
+
+print(env.observation_space.high)
+print(env.observation_space.low)
+
+DISCRETE_OS_SIZE = [20, 20]
+discrete_os_win_size = (env.observation_space.high - env.observation_space.low)/DISCRETE_OS_SIZE
+
+q_table = np.random.uniform(low=-2, high=0, size=(DISCRETE_OS_SIZE + [env.action_space.n]))
+
+done = False
+while not done:
+    action = 2
+    new_state, reward, done, _, _ = env.step(action)
+    print(reward, new_state)
